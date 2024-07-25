bdb10e0126eede8f0ac9f8f4cdf1f46e6ce85967
tw0ten <twoten.eq@gmail.com>
2024-07-25 18:30:21 +0300

    st

--- config/nvim/lua/set.lua
+++ config/nvim/lua/set.lua
@@ -6,11 +6,10 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
+vim.opt.expandtab = false
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
--- script/drpc/drpc.py
+++ script/drpc/drpc.py
@@ -1,29 +1,24 @@
-from tendo import singleton
-singleton.SingleInstance()
 from pypresence import Presence
 import time
 import subprocess
 
-user = subprocess.run("whoami", shell=True, capture_output=True, check=True)
-host = subprocess.run("uname -n", shell=True, capture_output=True, check=True)
-
-user = user.stdout.decode('utf-8').replace("\n", "")
-host = host.stdout.decode('utf-8')
+user = subprocess.run("whoami", shell=True, capture_output=True, check=True).stdout.decode('utf-8').strip()
+host = subprocess.run("uname -n", shell=True, capture_output=True, check=True).stdout.decode('utf-8')
 
 while True:
-	try:
-		rpc = Presence("1133070138602700810")
-		rpc.connect()
-		while True:
-			uptime = subprocess.run("uptime -p", shell=True, capture_output=True, check=True)
-			uptime = uptime.stdout.decode('utf-8')
-			print(rpc.update(
-				state = uptime,
-				details = user+"@"+host,
-				large_image = "arch",
-				small_image = "linux",
-				small_text = "linux"
-			))
-			time.sleep(4)
-	except:
-		print("discord disabled/error")
+    try:
+        rpc = Presence("1133070138602700810")
+        rpc.connect()
+        while True:
+            uptime = subprocess.run("uptime -p", shell=True, capture_output=True, check=True).stdout.decode('utf-8')
+            rpc.update(
+                state = uptime,
+                details = user+"@"+host,
+                large_image = "arch",
+                small_image = "linux",
+                small_text = "linux"
+            )
+            time.sleep(4)
+    except:
+        print("discord disabled/error")
+    time.sleep(4)
