ad0eb8cc1a6217d1e6129381efad514d93abf910
tw0ten <twoten.eq@gmail.com>
2024-08-06 22:57:18 +0300

    lc

--- config/nvim/lua/plugin/lsp.lua
+++ config/nvim/lua/plugin/lsp.lua
@@ -64,7 +64,7 @@ return {
 				vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
 				vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
 				vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-				vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+				vim.keymap.set("n", "<leader>vrf", function() vim.lsp.buf.references() end, opts)
 				vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 				vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
 				vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
