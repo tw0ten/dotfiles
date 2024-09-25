5e0b0dde12a749ec279a53cf3db82c37a5be694d
tw0ten <twoten.eq@gmail.com>
2024-09-25 21:49:18 +0300

    kw

--- etc/fehfetch/src/Args.java
+++ etc/fehfetch/src/Args.java
@@ -3,28 +3,28 @@ import java.util.Arrays;
 public class Args {
 	private final String[] args;
 
-	public Args(final String[] args){
+	public Args(final String[] args) {
 		this.args = args;
 	}
 
-	public boolean has(final String arg){
-		for(final String s : args){
-			if(arg.equals(s))
+	public boolean has(final String arg) {
+		for (final String s : args) {
+			if (arg.equals(s))
 				return true;
 		}
 		return false;
 	}
 
-	public String get(final String arg){
-		for(int i = args.length - 2; i >= 0; i--){
-			if(arg.equals(args[i]))
+	public String get(final String arg) {
+		for (int i = args.length - 2; i >= 0; i--) {
+			if (arg.equals(args[i]))
 				return args[i + 1];
 		}
 		return null;
 	}
 
 	@Override
-	public String toString(){
+	public String toString() {
 		return Arrays.toString(args);
 	}
 }
