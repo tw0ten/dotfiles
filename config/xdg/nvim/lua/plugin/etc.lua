9c2eee37f3425c6ca30ad1986d53c1bd0a8e6406
tw0ten <twoten.eq@gmail.com>
2025-07-03 14:20:58 +0300

    /y

---
+++ config/xdg/nvim/lua/plugin/etc.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {},
+				ignore_install = {},
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true,
+					disable = {},
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end
+	},
+}
