59c13cd6c0e511e5a8db3fffeaf56f87b26cb645
tw0ten <twoten.eq@gmail.com>
2026-03-08 11:36:27 +0000

    1772969787

---
+++ config/xdg/mozilla/firefox/profile/user.js
@@ -0,0 +1,6 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+user_pref("browser.startup.page", 3);
+
+user_pref("browser.urlbar.trimURLs", false);
+user_pref("browser.urlbar.trimHttps", false);
